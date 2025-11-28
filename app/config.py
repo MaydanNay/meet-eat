4bc64917e05239ed4e1202c90af6631acd038dc5
Maydan <donttouchegoista@gmail.com>
2025-11-29 01:03:53 +0500

    m

--- app/config.py
+++ app/config.py
@@ -3,5 +3,6 @@
 import os
 
 DB_PATH = os.getenv("DB_PATH", "db.sqlite3")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "8430676291:AAFr9yilHXr2Fel35y297btCjln6N6cR7l8")
+# BOT_TOKEN = os.getenv("BOT_TOKEN", "8430676291:AAFr9yilHXr2Fel35y297btCjln6N6cR7l8")
+BOT_TOKEN='8430676291:AAFr9yilHXr2Fel35y297btCjln6N6cR7l8'
 SERVER_BASE_URL = os.getenv("SERVER_BASE_URL", "")
